0815296e25e95f7eba7246c6613c44966cc2d75c
Chris Cheng <christophercheng@star1889.com>
2015-04-30 08:18:34 -0400

    View menu implemented

---
+++ Assets/CameraPositioning.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPositioning : MonoBehaviour {
+    public void HomeView() {
+        SetView(new Vector3(-20f, 0f, 0f));
+    }
+
+    public void OverheadView() {
+        SetView(new Vector3(-10f, 0f, 0f));
+    }
+
+    public void FrontView() {
+        SetView(new Vector3(-80f, 180f, 0f));
+    }
+
+    public void DriverView() {
+        SetView(new Vector3(-80f, 90f, 0f));
+    }
+
+    public void PassengerView() {
+        SetView(new Vector3(-80f, 270f, 0f));
+    }
+
+    public void RearView() {
+        SetView(new Vector3(-80f, 0f, 0f));
+    }
+
+    public void SetView(Vector3 euler) {
+        transform.position = Vector3.zero;
+        transform.eulerAngles = euler;
+    }
+}
