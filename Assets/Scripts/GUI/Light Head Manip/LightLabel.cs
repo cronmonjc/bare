be602d2b88b633a57426143d81c7640e4022565d
Chris Cheng <christophercheng@star1889.com>
2015-05-12 16:00:20 -0400

    Minor optimization with LightLabels.

--- Assets/Scripts/GUI/Light Head Manip/LightLabel.cs
+++ Assets/Scripts/GUI/Light Head Manip/LightLabel.cs
@@ -10,10 +10,40 @@ public class LightLabel : MonoBehaviour {
     private LightHead lh;
 
     public static CameraControl cam;
+    private StyleNode lastStyle;
 
     void Start() {
         selectionImage.gameObject.SetActive(false);
         selectionImage.transform.rotation = Quaternion.identity;
+        if(lh == null) lh = target.GetComponent<LightHead>();
+        if(lh.isSmall) {
+            ((RectTransform)transform).sizeDelta = new Vector2(65, 48);
+        }
+        Refresh();
+    }
+
+    public void Refresh() {
+        if(lh.lhd.style != null) {
+            label.text = "(" + lh.Bit + ")\n" + lh.lhd.style.name + " " + lh.lhd.optic.name;
+            Color clr = lh.lhd.style.color;
+            background.color = clr;
+            if(clr.r + clr.g < clr.b) {
+                label.color = Color.white;
+            } else {
+                label.color = Color.black;
+            }
+            if(lh.lhd.style.isDualColor) {
+                clr = lh.lhd.style.color2;
+            }
+            secondImage.color = clr;
+        } else {
+            label.text = "(" + lh.Bit + ")\nEmpty";
+            label.color = Color.white;
+            background.color = new Color(0, 0, 0, 0.45f);
+            secondImage.color = new Color(0, 0, 0, 0.45f);
+        }
+
+        lastStyle = lh.lhd.style;
     }
 
     void Update() {
@@ -31,38 +61,16 @@ public class LightLabel : MonoBehaviour {
         }
 
         if(lh == null) lh = target.GetComponent<LightHead>();
-        else {
-            if(lh.isSmall) {
-                ((RectTransform)transform).sizeDelta = new Vector2(65, 48);
-            }
+        if(lh.lhd.style != lastStyle) {
+            Refresh();
+        }
 
-            if(lh.lhd.style != null) {
-                label.text = "(" + lh.Bit + ")\n" + lh.lhd.style.name + " " + lh.lhd.optic.name;
-                Color clr = lh.lhd.style.color;
-                background.color = clr;
-                if(clr.r + clr.g < clr.b) {
-                    label.color = Color.white;
-                } else {
-                    label.color = Color.black;
-                }
-                if(lh.lhd.style.isDualColor) {
-                    clr = lh.lhd.style.color2;
-                }
-                secondImage.color = clr;
-            } else {
-                label.text = "(" + lh.Bit + ")\nEmpty";
-                label.color = Color.white;
-                background.color = new Color(0, 0, 0, 0.45f);
-                secondImage.color = new Color(0, 0, 0, 0.45f);
-            }
-
-            if(lh.Selected) {
-                selectionImage.gameObject.SetActive(true);
-                selectionImage.transform.Rotate(new Vector3(0, 0, 20f) * Time.deltaTime);
-            } else {
-                selectionImage.gameObject.SetActive(false);
-                selectionImage.transform.rotation = Quaternion.identity;
-            }
+        if(lh.Selected) {
+            selectionImage.gameObject.SetActive(true);
+            selectionImage.transform.Rotate(new Vector3(0, 0, 20f) * Time.deltaTime);
+        } else {
+            selectionImage.gameObject.SetActive(false);
+            selectionImage.transform.rotation = Quaternion.identity;
         }
     }
 }
