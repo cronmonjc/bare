55f4cef074a0e74f8ddf1c9561c984c33b73f4fe
Chris Cheng <christophercheng@star1889.com>
2015-06-01 15:59:47 -0400

    Minor Scene tweaks, and a button to override wiring

---
+++ Assets/OverrideWiring.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OverrideWiring : MonoBehaviour {
+    public Text t;
+    private Button b;
+
+    public void Clicked() {
+        LightLabel.wireOverride = !LightLabel.wireOverride;
+    }
+
+    void Update() {
+        if(b == null) b = GetComponent<Button>();
+
+        t.text = (LightLabel.wireOverride ? "YES" : "NO");
+
+        ColorBlock cb = b.colors;
+
+        cb.normalColor = (LightLabel.wireOverride ? Color.green : Color.red);
+        cb.highlightedColor = (LightLabel.wireOverride ? new Color(0.25f, 0.9f, 0.25f) : new Color(0.9f, 0.25f, 0.25f));
+        cb.pressedColor = (LightLabel.wireOverride ? new Color(0f, 0.75f, 0f) : new Color(0.75f, 0f, 0f));
+
+        b.colors = cb;
+    }
+}
