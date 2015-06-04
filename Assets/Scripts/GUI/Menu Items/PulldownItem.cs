2fdda3702b7770b266b68298453f61dedaacbdca
Chris Cheng <christophercheng@star1889.com>
2015-06-04 08:34:53 -0400

    Tweaks to PulldownItem to save memory

--- Assets/Scripts/GUI/Menu Items/PulldownItem.cs
+++ Assets/Scripts/GUI/Menu Items/PulldownItem.cs
@@ -8,12 +8,18 @@ public abstract class PulldownItem : MonoBehaviour {
     public ColorBlock selected = new ColorBlock() { colorMultiplier = 1f, normalColor = new Color32(255, 192, 0, 255), highlightedColor = new Color32(255, 223, 127, 255), pressedColor = new Color32(255, 192, 0, 255), disabledColor = new Color32(255, 144, 144, 255) },
                     unselected = new ColorBlock() { colorMultiplier = 1f, normalColor = new Color32(255, 255, 255, 255), highlightedColor = new Color32(245, 245, 245, 255), pressedColor = new Color32(200, 200, 200, 255), disabledColor = new Color32(255, 144, 144, 255) };
     private Button b;
+    private bool prev = false;
 
     protected abstract bool IsSelected();
     public abstract void Clicked();
 
     void Update() {
         if(b == null) b = GetComponent<Button>();
-        b.colors = IsSelected() ? selected : unselected;
+        bool curr = IsSelected();
+        if(prev ^ curr) {
+            b.colors = curr ? selected : unselected;
+            prev = curr;
+        }
+        
     }
 }
