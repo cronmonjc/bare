873c3c225410ce96e3d5caa49b8f9263fe362e04
Chris Cheng <christophercheng@star1889.com>
2015-06-05 16:12:26 -0400

    Last bit of work on the Advanced Functions for the week

--- Assets/Scripts/GUI/Light Head Manip/LightInteractionPanel.cs
+++ Assets/Scripts/GUI/Light Head Manip/LightInteractionPanel.cs
@@ -3,32 +3,54 @@ using System.Collections;
 
 public class LightInteractionPanel : MonoBehaviour {
     private enum ShowState {
-        SUMMARY, OPTICS
+        SUMMARY, OPTICS, FUNCASSIGN, FUNCEDIT
     }
     private static CameraControl cam;
-    private ShowState state = ShowState.SUMMARY;
-    public GameObject SummaryPane, OpticPane;
+    public static bool EditingFunc = false;
+    private ShowState _state = ShowState.SUMMARY;
+    private ShowState state {
+        set { _state = value; Set(); }
+        get { return _state; }
+    }
+    public GameObject SummaryPane, OpticPane, FuncAssignPane, FuncEditPane;
 
     // Update is called once per frame
     void Update() {
         if(cam == null) {
             cam = FindObjectOfType<CameraControl>();
-            SummaryPane.SetActive(true);
-            OpticPane.SetActive(false);
+            Set();
         }
 
-        if(cam.OnlyCamSelected.Count == 0) {
-            if(state != ShowState.SUMMARY) {
-                state = ShowState.SUMMARY;
-                SummaryPane.SetActive(true);
-                OpticPane.SetActive(false);
-            }
-        } else {
-            if(state != ShowState.OPTICS) {
-                state = ShowState.OPTICS;
-                SummaryPane.SetActive(false);
-                OpticPane.SetActive(true);
-            }
+        switch(state) {
+            case ShowState.SUMMARY:
+                if(cam.OnlyCamSelected.Count > 0) {
+                    state = ShowState.OPTICS;
+                } else if(EditingFunc) {
+                    state = ShowState.FUNCASSIGN;
+                }
+                break;
+            case ShowState.OPTICS:
+                if(cam.OnlyCamSelected.Count == 0) {
+                    state = ShowState.SUMMARY;
+                } else if(EditingFunc) {
+                    state = ShowState.FUNCASSIGN;
+                }
+                break;
+            case ShowState.FUNCASSIGN:
+                if(!EditingFunc) {
+                    state = ShowState.SUMMARY;
+                }
+                break;
+            case ShowState.FUNCEDIT:
+            default:
+                break;
         }
+    }
+
+    private void Set() {
+        SummaryPane.SetActive(state == ShowState.SUMMARY);
+        OpticPane.SetActive(state == ShowState.OPTICS);
+        FuncAssignPane.SetActive(state == ShowState.FUNCASSIGN);
+        FuncEditPane.SetActive(state == ShowState.FUNCEDIT);
     }
 }
