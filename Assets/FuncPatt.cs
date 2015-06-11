408a164e4aaad73f59b6a19ee31dc46bcde326e1
Chris Cheng <christophercheng@star1889.com>
2015-06-11 15:57:39 -0400

    More Advanced work
    Finally starting to work on the display/selection of the different
    patterns

---
+++ Assets/FuncPatt.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FuncPatt : MonoBehaviour {
+    public Text text;
+    public Pattern patt;
+    public Image i;
+    public FuncPattSelect fps;
+
+    void Start() {
+        text.text = patt.name;
+    }
+
+    void Update() {
+        bool on = true;
+
+        foreach(LightHead alpha in BarManager.inst.allHeads) {
+
+        }
+    }
+
+    public void Clicked() {
+        fps.SetSelection(patt);
+    }
+}
---
+++ Assets/FuncPattSelect.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FuncPattSelect : MonoBehaviour {
+    public RectTransform menu;
+    public GameObject optionPrefab;
+    public CameraControl cam;
+    public bool IsTD = false;
+    public bool IsColor2 = false;
+
+    public void Clear() {
+        List<Transform> temp = new List<Transform>();
+        foreach(Transform alpha in menu) {
+            temp.Add(alpha);
+        }
+        foreach(Transform alpha in temp) {
+            DestroyImmediate(alpha.gameObject);
+        }
+    }
+
+    public void Refresh() {
+        Clear();
+
+        if(IsTD) {
+        
+        } else {
+            foreach(FlashPatt alpha in LightDict.inst.flashPatts) {
+                GameObject newbie = GameObject.Instantiate<GameObject>(optionPrefab);
+                newbie.transform.SetParent(menu, false);
+                newbie.transform.localScale = Vector3.one;
+                FuncPatt patt = newbie.GetComponent<FuncPatt>();
+                patt.fps = this;
+                patt.patt = alpha;
+            }
+        }
+    }
+
+    public void SetSelection(Pattern p) {
+
+    }
+}
