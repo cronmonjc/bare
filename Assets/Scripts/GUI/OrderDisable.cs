4ba2d8e1cd38d5b927bfcaf1bcecfe8c547c59cd
Chris Cheng <christophercheng@star1889.com>
2015-07-02 11:24:52 -0400

    Name and PO required to publish

---
+++ Assets/Scripts/GUI/OrderDisable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OrderDisable : MonoBehaviour {
+    public Button b;
+    public InputField name, po;
+
+    public void Test() {
+        b.interactable = (name.text != "" && po.text != "");
+    }
+}
