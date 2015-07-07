aaff979734f23fc63e1e5657956212c3e01c0bc3
Chris Cheng <christophercheng@star1889.com>
2015-07-07 09:53:38 -0400

    Added a Program's Directory link to File Browser

---
+++ Assets/Scripts/GUI/File Browser/Quick Links/ProgramDirLink.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProgramDirLink : DirectoryLink {
+    public override void Navigate(FileBrowser fb) {
+        fb.Navigate(BarManager.DirRoot);
+    }
+}
