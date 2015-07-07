ac35f5e221efc294ff2c4efeb34793c89861c0a6
Chris Cheng <christophercheng@star1889.com>
2015-07-07 14:10:59 -0400

    Error Text fade

--- Assets/Scripts/GUI/ErrorText.cs
+++ Assets/Scripts/GUI/ErrorText.cs
@@ -14,7 +14,25 @@ public class ErrorText : MonoBehaviour {
     }
 
     public void DispError(string msg) {
+        StartCoroutine(DispErrorThenFade(msg));
+    }
+
+    public IEnumerator DispErrorThenFade(string msg) {
+        Color32 c = t.color;
+        c.a = 255;
+        t.color = c;
         t.text = msg;
+
+        yield return new WaitForSeconds(5f);
+
+        for(; (c.a - 4) > 0f; c.a -= 4) {
+            t.color = c;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        t.text = "";
+        c.a = 255;
+        t.color = c;
     }
     
 }
