b8da1d5cae3d468100cb2aca332b814603329d81
Chris Cheng <christophercheng@star1889.com>
2015-07-22 15:18:59 -0400

    Added OutputOverload Issue, tweaked appearance of Files

---
+++ Assets/Scripts/GUI/Issues/OutputOverload.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OutputOverload : IssueChecker {
+    public uint maxAllowablePerOutput = 2500;
+
+    private Dictionary<byte, uint> map;
+
+    public override string pdfText {
+        get { return ""; }
+    }
+
+    public override bool DoCheck() {
+        if(map == null) map = new Dictionary<byte, uint>();
+
+        for(byte i = 0; i < 32; i++) {
+            map[i] = 0;
+        }
+
+        LightHead alpha;
+        byte thisBit = 0;
+        for(byte i = 0; i < BarManager.inst.allHeads.Count; i++) {
+            alpha = BarManager.inst.allHeads[i];
+            if(!alpha.gameObject.activeInHierarchy) continue;
+            if(!alpha.hasRealHead) continue;
+            if(alpha.Bit == 255) continue;
+
+            thisBit = (byte)(alpha.Bit + (alpha.isRear ? 16 : 0));
+            map[thisBit] += alpha.lhd.optic.amperage;
+            if(map[thisBit] > maxAllowablePerOutput) return true;
+        }
+        return false;
+    }
+}
