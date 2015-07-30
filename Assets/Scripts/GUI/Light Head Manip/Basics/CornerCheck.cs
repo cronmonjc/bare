cb4d06ca299d416c179b01bae8e291871aeafd79
Chris Cheng <christophercheng@star1889.com>
2015-07-30 11:50:21 -0400

    Basic Optic editing now pays attention to whether or not you have optics selected

---
+++ Assets/Scripts/GUI/Light Head Manip/Basics/CornerCheck.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CornerCheck : MonoBehaviour {
+    public GameObject CornerLong, RearCorner;
+
+    public void Retest() {
+        foreach(LightHead alpha in BarManager.inst.allHeads) {
+            if(!alpha.gameObject.activeInHierarchy || !alpha.Selected) continue;
+
+            CornerLong.SetActive(false);
+            RearCorner.SetActive(false);
+
+            if(alpha.loc == Location.FRONT_CORNER || alpha.loc == Location.REAR_CORNER) {
+                CornerLong.SetActive(true);
+            }
+            if(alpha.loc == Location.REAR_CORNER) {
+                RearCorner.SetActive(true);
+            }
+        }
+    }
+}
