a03467f2887257858d5589e09bf7c9841ea6285f
The Stick <avaera8820@gmail.com>
2015-08-10 20:55:38 -0400

    Some minor documentation on small fries

--- Assets/Scripts/GoStraightToScene.cs
+++ Assets/Scripts/GoStraightToScene.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Small Component that forces the Application to move to another Scene immediately.  Used for the Splash Screen.
+/// </summary>
 public class GoStraightToScene : MonoBehaviour {
     /// <summary>
     /// Start is called once, when the containing GameObject is instantiated, after Awake.
